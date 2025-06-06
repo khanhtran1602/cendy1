ee2eec438dc210bcb3bf552dad3835670a3317ae
kkkasdoas <vothanhlan2909@gmail.com>
2025-06-06 09:11:59 +0700

    change not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFoundScreen() {
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist.</ThemedText>
         <Link href="/(tabs)/home" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="link">Go to home screen ok luon!</ThemedText>
         </Link>
       </ThemedView>
     </>
